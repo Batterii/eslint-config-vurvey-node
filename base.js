062299a0b5d8d21f08365a335640ae980c5917a9
Steve Ripberger <sripberger@batterii.com>
2021-11-01 14:21:24 -0400

    Add eslint-config-node plugin and node-specific rules from base

--- base.js
+++ base.js
@@ -2,18 +2,23 @@ module.exports = {
 	env: {
 		node: true,
 	},
-	extends: "@batterii/eslint-config-vurvey/base",
+	extends: [
+		"@batterii/eslint-config-vurvey/base",
+		"plugin:node/recommended",
+	],
 	plugins: [
 		"import",
 		"sort-imports-es6-autofix",
 	],
 	rules: {
 		"import/extensions": ["error", "always", {ignorePackages: true}],
+		"no-process-env": "error",
+		"no-process-exit": "error",
 		"no-restricted-syntax": ["error", {
 			message: "Don't declare enums",
 			selector: "TSEnumDeclaration",
 		}],
-		"sort-imports": "off",
+		"no-sync": "warn",
 		"sort-imports-es6-autofix/sort-imports-es6": ["error", {
 			ignoreCase: true,
 			ignoreMemberSort: false,
